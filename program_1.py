add3a3bf9d2e2e5702c48fc627ca8c86e88fa3f3
LorynnPerell <lcperell@students.unwsp.edu>
2025-10-04 04:07:43 +0000

    Lp, 10/3/25, Assginment 5

--- program_1.py
+++ program_1.py
@@ -8,23 +8,15 @@
 def kilometer_conversion(kilometers):    
     miles = 0.0
     ######################
-def askUserForKilometers():
-    userKilometers = float( input( "Please enter the distance" + " in kilometers: " ))
-    return userKilometers
-def convertKilometersToMiles( userKilometers ):
-    miles = userKilometers * 0.6214
+    miles = kilometers * 0.6214
     return miles
 def main():
-    userKilometersTyped = askUserForKilometers()
-    convertedMiles = convertKilometersToMiles( userKilometersTyped )
-
-    print( "\n", userKilometersTyped, "Kilometers converted to miles is", \
-          format( convertedMiles, ".2f" ), "miles", sep="" )
-    
-main()
+    km = float(input("Enter distance in kilometers: "))
+    miles = kilometer_conversion(km)
+    print(f"{km} kilometers is equal to {miles:.2f} miles.")
     ######################    
 
-
+main()
     # Return the variable to the calling function
     
 
